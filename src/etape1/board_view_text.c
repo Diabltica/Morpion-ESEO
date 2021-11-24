9c73d81d55e10b6eed335c837fcaa78cfd448db6
Diabltica <Diabltica@gmail.com>
2021-11-24 10:51:44 +0100

    add display players turn

--- src/etape1/board_view_text.c
+++ src/etape1/board_view_text.c
@@ -4,37 +4,47 @@
 
 #if defined CONFIG_TEXTUI
 
-void BoardView_init (void)
+PieceType boardSquares[3][3];
+char boardDisplay[5][5];
+
+void BoardView_init(void)
 {
+
+
+void BoardView_free(void) {}
+
+void BoardView_displayAll(void)
+{
+	//TODO
 }
 
-void BoardView_free (void)
-{
-}
-
-void BoardView_displayAll (void)
-{
-  // TODO: à compléter
-}
-
-void BoardView_displaySquare (Coordinate x, Coordinate y, PieceType kindOfPiece)
+void BoardView_displaySquare(Coordinate x, Coordinate y, PieceType kindOfPiece)
 {
 	BoardView_displayAll();
 }
 
-void BoardView_displayEndOfGame (GameResult result)
+void BoardView_displayEndOfGame(GameResult result)
 {
-  // TODO: à compléter
+	// TODO: à compléter
 }
 
-void BoardView_displayPlayersTurn (PieceType thisPlayer)
+void BoardView_displayPlayersTurn(PieceType thisPlayer)
 {
-  // TODO: à compléter
+	switch (thisPlayer) {
+		case CROSS:
+			printf("It's the turn of cross");
+			break;
+		case CIRCLE:
+			printf("It's the turn of circle");
+			break;
+		case NONE:
+			break;
+	}
 }
 
-void BoardView_sayCannotPutPiece (void)
+void BoardView_sayCannotPutPiece(void)
 {
-  // TODO: à compléter
+	// TODO: à compléter
 }
 
-#endif // defined CONFIG_TEXTUI
+#endif// defined CONFIG_TEXTUI
