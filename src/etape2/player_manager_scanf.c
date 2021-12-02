b92c8edfff3ff45f456a0561074cf8ce69413ac1
sarlatme <meven.sarlat@reseau.eseo.fr>
2021-12-02 16:42:26 +0100

    add scanf with using condition

--- src/etape2/player_manager_scanf.c
+++ src/etape2/player_manager_scanf.c
@@ -12,8 +12,11 @@
 
 #if defined CONFIG_PLAYER_MANAGER_SCANF
 
+PieceType thisPlayer;
+
 void PlayerManager_init (void)
 {
+	thisPlayer = CROSS;
 }
 
 void PlayerManager_free (void)
@@ -22,7 +25,39 @@ void PlayerManager_free (void)
 
 void PlayerManager_oneTurn (void)
 {
-  // TODO: à compléter
+	int error;
+	char row;
+	char col;
+
+	//comparaison avec le code ASCII
+	do{
+		printf("Wich row ? ");
+		error = scanf("%c", &row);
+		getchar();
+	}while(error != 1 || row > 50 || row < 48);
+
+	//comparaison avec le code ASCII
+	do{
+		printf("Wich columne ? ");
+		error = scanf("%c", &col);
+		getchar();
+	}while(error != 1 || col > 50 || col < 48);
+
+	//Put piece on board
+	Board_putPiece(row,col,thisPlayer);
+
+	//next player
+	switch (thisPlayer) {
+		case CROSS:
+			thisPlayer = CIRCLE;
+			break;
+		case CIRCLE:
+			thisPlayer = CROSS;
+			break;
+		default:
+			printf("Error during change of player.");
+	}
+
 }
 
 #endif // defined CONFIG_PLAYER_MANAGER_SCANF
