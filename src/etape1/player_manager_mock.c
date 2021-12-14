20923e81eff142996ed9171f8e341cb1215ca44b
Diabltica <Diabltica@gmail.com>
2021-12-14 16:42:08 +0100

    add if defined to test all end of game case

--- src/etape1/player_manager_mock.c
+++ src/etape1/player_manager_mock.c
@@ -24,12 +24,21 @@ void PlayerManager_free(void) {}
 
 void PlayerManager_oneTurn(void)
 {
+#if defined DEF_CIRCLE_WINS
 	//Circle Win test set
-	int test_x[6] = {0, 1, 0, 1, 2, 1};
+	int test_x[6] = {0, 1, 0, 1, 2, 1};//row
 	int test_y[6] = {0, 0, 1, 1, 2, 2};
+#endif
+#if defined DEF_CROSS_WINS
+	//Circle Win test set
+	int test_x[5] = {0, 1, 0, 1, 0}; //row
+	int test_y[5] = {0, 0, 1, 1, 2};
+#endif
+#if defined DEF_DRAW
 	//Draw Test set
-//	int test_x[9] = {0,0,1,2,1,0,2,1,2};
-//	int test_y[9] = {1,0,0,1,1,2,0,2,2};
+	int test_x[9] = {0,0,1,2,1,0,2,1,2};//row
+	int test_y[9] = {1,0,0,1,1,2,0,2,2};
+#endif
 
 
 	Board_putPiece(test_x[i], test_y[i], thisPlayer);
