f2c2aad36784c9cf4cfe334dc9ed245efb67157e
Diabltica <Diabltica@gmail.com>
2021-12-15 15:22:48 +0100

    add comment

--- src/etape2/player_manager_scanf.c
+++ src/etape2/player_manager_scanf.c
@@ -12,12 +12,13 @@
 
 #if defined CONFIG_PLAYER_MANAGER_SCANF
 
-PieceType thisPlayer;
-void PlayerManager_interaction(int* x, int* y);
+PieceType thisPlayer; //declare variable to know who play
+void PlayerManager_interaction(int* x, int* y); //definition function who
+// handle interaction with players
 
 void PlayerManager_init (void)
 {
-	thisPlayer = CROSS;
+	thisPlayer = CROSS; //Choose the first player
 	BoardView_displayAll(); // Display a first time the board
 }
 
@@ -28,25 +29,27 @@ void PlayerManager_free (void)
 void PlayerManager_oneTurn (void)
 {
 
-	BoardView_displayPlayersTurn(thisPlayer);
+	BoardView_displayPlayersTurn(thisPlayer); //display who need to play
 
+	//declare buffer for coordinate
 	int x = 0;
 	int y = 0;
 
-	PlayerManager_interaction(&x, &y);
+	PlayerManager_interaction(&x, &y); //ask player coordinate to put his piece
 
-	//Put piece on boardgi
-	bool is_put = false;
+	//Put piece on board
+	bool is_put = false; // to know if the piece is placed
 	do{
+		//try to put the piece
 		PutPieceResult PieceResult = Board_putPiece(x,y,thisPlayer);
-		if(PieceResult == SQUARE_IS_NOT_EMPTY){
-			BoardView_sayCannotPutPiece();
-			PlayerManager_interaction(&x, &y);
-		}else if(PieceResult == PIECE_IN_PLACE){
-			is_put = true;
+		if(PieceResult == SQUARE_IS_NOT_EMPTY){ // If the square is not empty
+			BoardView_sayCannotPutPiece();//display msg
+			PlayerManager_interaction(&x, &y);//ask new coordinate
+		}else if(PieceResult == PIECE_IN_PLACE){// If the square is empty
+			is_put = true;//Piece placed
 		}
 
-	}while(!is_put);
+	}while(!is_put); //while the piece is not placed
 
 	//next player
 	switch (thisPlayer) {
@@ -68,23 +71,23 @@ void PlayerManager_interaction(int* x, int* y){
 	char row;
 	char col;
 
-	//comparaison avec le code ASCII
+	//Compare with ASCII Code
 	do{
 		printf("Which row ? ");
 		error = scanf("%c", &row);
 		getchar();
-	}while(error != 1 || row > 50 || row < 48);
+	}while(error != 1 || row > 50 || row < 48); //While the input is wrong
 
-	*x = row - 48; //on retrouve le nombre entier
+	*x = row - 48; //get correct coordinate
 
-	//comparaison avec le code ASCII
+	//Compare with ASCII Code
 	do{
 		printf("Which column ? ");
 		error = scanf("%c", &col);
 		getchar();
-	}while(error != 1 || col > 50 || col < 48);
+	}while(error != 1 || col > 50 || col < 48);//While the input is wrong
 
-	*y = col - 48;
+	*y = col - 48;//get correct coordinate
 
 }
 
