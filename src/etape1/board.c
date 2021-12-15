1342f64d00a6b95b4faf72d4e926baeb92474511
Diabltica <Diabltica@gmail.com>
2021-12-15 09:05:06 +0100

    add comment

--- src/etape1/board.c
+++ src/etape1/board.c
@@ -1,13 +1,14 @@
 #include "board.h"
 #include <stdbool.h>
 
+//define the size of the board
 #define LIGNES 3
 #define COLONNES 3
 
-PieceType boardSquares[LIGNES][COLONNES];
-SquareChangeCallback boardOnSquareChange;
-EndOfGameCallback boardOnEndOfGame;
-GameResult boardGameResult;
+PieceType boardSquares[LIGNES][COLONNES]; //declare the board
+SquareChangeCallback boardOnSquareChange; //declare OnSquareChange callback
+EndOfGameCallback boardOnEndOfGame;//declare EndOfGame Callback
+GameResult boardGameResult; //declare the gameresult variable
 
 
 /**
@@ -33,33 +34,39 @@ static bool isGameFinished(const PieceType boardSquares[3][3],
 {
 
 	//check if the board is full
-	bool stop = false;
-
-	for (int i = 0; (i < LIGNES) && !stop; i++) {
-		for (int j = 0; (j < COLONNES) && !stop; j++) {
-			if (boardSquares[i][j] == NONE) { stop = true; }
+	bool stop = true;
+	for (int i = 0; (i < LIGNES) && stop; i++) {
+		for (int j = 0; (j < COLONNES) && stop; j++) {
+			if (boardSquares[i][j] == NONE) {
+				stop = false;
+			}
 		}
 	}
-	if (!stop) {
+	if (stop) { //if the board is full return DRAW
 		*gameResult = DRAW;
 		return true;
 	}
 
-	// look at the diagonale, ligne and colomne of the last change to check a win condition
+	// Check if the last piece played win the game
+	//Check for columns
 	if (boardSquares[lastChangeY][0] == boardSquares[lastChangeY][1] &&
 	            boardSquares[lastChangeY][1] == boardSquares[lastChangeY][2] &&
 	            boardSquares[lastChangeY][2] != NONE ||
+	    //Check for row
 	    boardSquares[0][lastChangeX] == boardSquares[1][lastChangeX] &&
 	            boardSquares[1][lastChangeX] == boardSquares[2][lastChangeX] &&
 	            boardSquares[2][lastChangeX] != NONE ||
+	    //Check for diagonal (top left to bottom right)
 	    lastChangeX == lastChangeY &&
 	            boardSquares[0][0] == boardSquares[1][1] &&
 	            boardSquares[1][1] == boardSquares[2][2] &&
 	            boardSquares[2][2] != NONE ||
+	    //Check for the other diagonal (bottom left to top right)
 	    lastChangeX + lastChangeY == 2 &&
 	            boardSquares[0][2] == boardSquares[1][1] &&
 	            boardSquares[1][1] == boardSquares[2][0] &&
 	            boardSquares[2][0] != NONE) {
+		//Check who's the winner and change *gameResult
 		switch (boardSquares[lastChangeY][lastChangeX]) {
 			case CROSS:
 				*gameResult = CROSS_WINS;
@@ -73,47 +80,47 @@ static bool isGameFinished(const PieceType boardSquares[3][3],
 		}
 		return true;
 	} else {
-		*gameResult = DRAW;
+		*gameResult = DRAW;//return Draw if the game is not ended
 		return false;
 	}
 }
 
 
 void Board_init(SquareChangeCallback onSquareChange,
-                EndOfGameCallback onEndOfGame)
+                EndOfGameCallback onEndOfGame)//init the board
 {
 	for (int i = 0; i < LIGNES; ++i) {
 		for (int j = 0; j < COLONNES; ++j) { boardSquares[i][j] = NONE; }
-	}
-	boardOnSquareChange = onSquareChange;
-	boardOnEndOfGame = onEndOfGame;
+	}                                    // initialize all square to NONE
+	boardOnSquareChange = onSquareChange;//init the OnSquareChange callback
+	boardOnEndOfGame = onEndOfGame;      // init the EndOfGame callback
 }
 
 void Board_free()
 {
 	for (int i = 0; i < LIGNES; ++i) {
 		for (int j = 0; j < COLONNES; ++j) { boardSquares[i][j] = NONE; }
-	}
+	}// Free the board by putting all square to None
 }
 
-PutPieceResult Board_putPiece(Coordinate x, Coordinate y, PieceType
-                                                                    kindOfPiece)
+PutPieceResult Board_putPiece(Coordinate x, Coordinate y, PieceType kindOfPiece)
 {
 	PutPieceResult Is_empty;
 
-	if (Board_getSquareContent(x, y) == NONE) {
-		boardSquares[y][x] = kindOfPiece;
-		boardOnSquareChange(x, y, kindOfPiece);
+	if (Board_getSquareContent(x, y) == NONE) { //check if the square is empty
+		boardSquares[y][x] = kindOfPiece; // Put the piece
+		boardOnSquareChange(x, y, kindOfPiece);//Call OnSquareChange callback
+		//Check if the game is win
 		if (isGameFinished(boardSquares, x, y, &boardGameResult))
-			boardOnEndOfGame(boardGameResult);
-		Is_empty = PIECE_IN_PLACE;
+			boardOnEndOfGame(boardGameResult);//Call EndOfGame callback
+		Is_empty = PIECE_IN_PLACE; //save that the piece is in place
 	} else {
-		Is_empty = SQUARE_IS_NOT_EMPTY;
+		Is_empty = SQUARE_IS_NOT_EMPTY; //save that the square is occupied
 	}
-	return Is_empty;
+	return Is_empty; //return the value saved
 }
 
 PieceType Board_getSquareContent(Coordinate x, Coordinate y)
 {
-	return boardSquares[y][x];
+	return boardSquares[y][x]; // return the content of the square
 }
